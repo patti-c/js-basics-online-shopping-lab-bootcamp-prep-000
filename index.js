161a965f0dde8a7907cf2ed2b60d9eaf44c44ccc
Patricia Carter <patricia4194@gmail.com>
2018-07-13 15:42:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,5 +80,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if(cardNumber === null) {
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    var totalCost = total();
+    return `Your total cost is ${totalCost}, which will be charged to the card ${cardNumber}.`;
+  }
 }
